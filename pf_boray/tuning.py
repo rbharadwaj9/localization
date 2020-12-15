a3840c76c0c5a0b8aca0412e93e183e67a52c793
jared12222 <jared12222@gmail.com>
2020-12-14 20:40:47 -0500

    Modify to be compatible to our owned data

--- pf_boray/tuning.py
+++ pf_boray/tuning.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fitting():    
+def fitting(filename):    
     # model
     A = np.eye(2)
     B = np.matrix("1.5 0.1; 0.2 -0.5")
@@ -12,27 +12,31 @@ def fitting():
     C = np.matrix("1.05 0.01; 0.01 0.9")
 
     # load data
-    with open("kfdata.dat","rb") as f:
-        noisy_measurement,actions,ground_truth_states,N = pickle.load(f)
+    with open(filename,"rb") as f:
+        in_dict = pickle.load(f)
+        noisy_measurement = in_dict['sensor_data']
+        actions = in_dict['action']
+        ground_truth_states = in_dict['groundtruth']
+        N = noisy_measurement.shape[0]
 
     motion_errors = np.zeros((2,N))
     sensor_errors = np.zeros((2,N))
     for i in range(1,N):
-        x_t = np.matrix(ground_truth_states[:,i]).transpose()
-        x_tminus1 = np.matrix(ground_truth_states[:,i-1]).transpose()
-        u_t = np.matrix(actions[:,i]).transpose()
-        z_t = np.matrix(noisy_measurement[:,i]).transpose()
+        x_t = np.matrix(ground_truth_states[i,:]).transpose()
+        x_tminus1 = np.matrix(ground_truth_states[i-1,:]).transpose()
+        u_t = np.matrix(actions[i,:]).transpose()
+        z_t = np.matrix(noisy_measurement[i,:]).transpose()
         
         motion_errors[:,i] = np.squeeze(x_t - A*x_tminus1 - B*u_t)
         sensor_errors[:,i] = np.squeeze(z_t - C*x_t)
     
     motion_cov=np.cov(motion_errors)
-    sensor_cov=np.cov(sensor_errors)
+    sensor_cov=np.cov(in_dict['sensor_noise'].transpose())
 
     return motion_cov,sensor_cov
 
 if __name__ == "__main__":
-    motion_cov,sensor_cov = fitting()
+    motion_cov,sensor_cov = fitting("/home/boray/Intro_Algorithmic_Robot/eecs498-localization/astar_path.pickle")
     
     print "Motion cov:"
     print motion_cov
